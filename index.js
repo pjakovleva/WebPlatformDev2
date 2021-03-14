d9692b0748f0f69073129d3bfdd6de6023f42392
pjakovleva <pjjakovleva@gmail.com>
2021-03-14 12:33:55 +0000

    Added index.js file contents

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = require('./routes/plannerRoutes.js');
+const path = require('path');
+const mustache = require('mustache-express');
+const bodyParser = require('body-parser')
+
+const app = express();
+
+const public = path.join(__dirname, 'public');
+app.use(express.static('public'));
+
+app.engine('mustache', mustache());
+app.set('view engine', 'mustache');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', router);
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000. Ctrl^c to quit.');
+});
